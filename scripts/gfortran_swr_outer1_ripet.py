de20abae8213c065da2c1710381786d9c02b9971
gyanz <gyanBasyalz@gmail.com>
2018-01-15 19:28:04 -0700

    updated gfortran compile script

--- scripts/gfortran_swr_outer1_ripet.py
+++ scripts/gfortran_swr_outer1_ripet.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os
 import sys
 import shutil
+import traceback
 import pymake
 from pymake.download import download_and_unzip
 
@@ -30,16 +31,30 @@ def make_mfnwt(dl=True,init_del=True,final_del=True,clean=True):
     url = "https://github.com/gyanz/MODFLOW-NWT/archive/%s.zip"%suffix
 
     if dl:
-        download_and_unzip(url)
+        try:
+            download_and_unzip(url)
+        except:
+            print(traceback.print_exc())
+            return None
+            
 
     # Remove the parallel and serial folders from the source directory
     srcdir = os.path.join(dirname,'src')
     target = 'mfnwt'
 
 
-    pymake.main(srcdir, target, 'gfortran', 'gcc', makeclean=clean,
-                expedite=False, dryrun=False, double=False, debug=False,
-                fflags='cpp O2 fbacktrace ffpe-summary=overflow DSWR_OUTER_1 DRIP_ET')
+    try:
+        pymake.main(srcdir, target, 'gfortran', 'gcc', makeclean=clean,
+                    expedite=False, dryrun=False, double=False, debug=False,
+                    fflags='cpp ffpe-summary=overflow DSWR_OUTER_1 DRIP_ET')
+
+        # Clean up
+        if os.path.isdir(dirname) and final_del:
+            shutil.rmtree(dirname)
+
+    except:
+        print(traceback.print_exc())
+        return None
     
     if sys.platform == 'win32':
         try:
@@ -50,16 +65,13 @@ def make_mfnwt(dl=True,init_del=True,final_del=True,clean=True):
             
     assert os.path.isfile(target), 'Target %s does not exist.'%target
     
-    # Clean up
-    if os.path.isdir(dirname) and final_del:
-        shutil.rmtree(dirname)
 
 if __name__ == "__main__":
     args = sys.argv
     dl = 1   #download flag 
     init_del = 1 # delete initial files
     final_del = 1 # delete 
-    clean = 1 # clean make
+    clean = 0 # clean make
     arg_list = [dl,init_del,final_del,clean]
     for i,arg in enumerate(args[1:]):
         if int(arg) == 0:
